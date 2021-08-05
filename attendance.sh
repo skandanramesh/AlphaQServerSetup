da91fb29d5f0a99c1dad9b58faee3fd71a0d8eee
Skandan Ramesh <54405114+skandyman@users.noreply.github.com>
2021-08-05 22:50:38 +0530

    Update attendance.sh

--- attendance.sh
+++ attendance.sh
@@ -1,48 +1,48 @@
-#! /usr/bin/bash
+#! /bin/bash
 
-f = 0; str = "2019-09-27";
-d1 = "$1";
-d2 = "$2"; 
-
-if [ $# -eq 0 ]; then
-    d1 = "2019-09-27";
-    $2d = $(date +%F);
+if [[ $1 == "" ]] && [[ $2 == "" ]]
+then
+d1="2019-09-27"
+d2=$(date -I)
+elif [[ $2 == "" ]]
+then
+d1=$1
+d2=$(date -I)
+else
+d1=$1
+d2=$2
 fi
 
-for i in {01..30}
+while [ $d1 != $d2 ]
 do
-awk -F '[-,]' 
-	'{ if("$2" < "$d2" && "$2" > "$d1")
-		if("str != "$2" && f=0) 
-			{print $1" "$2; f=0;}
-		str = "$2"; 
-		if("$1" == "sysAd_$i") 
-			f=1;
-	}'
-attendance.log
 
 for i in {01..30}
 do
-awk -F '[-,]' 
-	'{ if("$2" < "$d2" && "$2" > "$d1")
-		if("str != "$2" && f=0) 
-			print $1" "$2; f=0;
-		str = "$2"; 
-		if("$1" == "webDev_$i") 
-			f=1;
-	}'
-attendance.log
-
-for i in {01..30}
-do
-awk -F '[-,]' 
-	'{ if("$2" < "$d2" && "$2" > "$d1")
-		if("str != "$2" && f=0) 
-			print $1" "$2; f=0;
-		str = "$2"; 
-		if("$1" == "appDev_$i") 
-			f=1;
-	}'
-attendance.log
+grep -q "$d1" ./attendance.log
+var1=$?
+if [ $var1 == 1 ]
+then 
+break
+fi
+grep -q "webDev_$i - $d1" ./attendance.log
+var2=$?
+grep -q "appDev_$i - $d1" ./attendance.log
+var3=$?
+grep -q "sysAd_$i - $d1" ./attendance.log
+var4=$?
+if [ $var1 != $var2 ]
+then
+echo "absent: webDev_$i - $d1"
+fi
+if [ $var1 != $var3 ]
+then
+echo "absent: appDev_$i - $d1"
+fi
+if [ $var1 != $var4 ]
+then
+echo "absent: sysAd_$i - $d1"
+fi
 done
+d1=$(date -I -d "$d1 + 1 day")
 
+done
